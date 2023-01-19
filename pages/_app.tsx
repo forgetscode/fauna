3d68a9db156d1a454211a8e13a9ca029ef87a7e4
forgetscode <forgetscode@gmail.com>
2023-01-18 18:29:26 -0800

    fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import * as React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Component { ...pageProps } />
+  );
 }
+
+export default MyApp;
